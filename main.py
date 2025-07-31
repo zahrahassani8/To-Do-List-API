8b3a58dcb06b75a9958e60eec416fba42b507545
zahrahassani8 <zahrahassanii08@gmail.com>
2025-07-31 17:30:46 +0330

    adding crud functionality

--- main.py
+++ main.py
@@ -38,3 +38,44 @@ class UpdateTodo(BaseModel):
 @app.get("/")
 async def home():
     return {"message": "FastAPI is working!"}
+
+
+@app.get("/todos", response_model=List[Todo], status_code=200)
+async def get_all_todos(limit: Optional[int] = None):
+    return TODOS[:limit] if limit else TODOS
+
+
+@app.get("/todos/{todo_id}", response_model=Todo, status_code=200)
+async def get_todo(todo_id: int):
+    for todo in TODOS:
+        if todo["id"] == todo_id:
+            return todo
+    raise HTTPException(status_code=404, detail="Not found.")
+
+
+@app.post("/todos", response_model=Todo, status_code=201)
+async def create_todo(todo: CreateTodo):
+    new_id = TODOS[-1]["id"] + 1 if TODOS else 1
+    new_todo = todo.dict()
+    new_todo["id"] = new_id
+    TODOS.append(new_todo)
+    return new_todo
+
+
+@app.put("/todos/{todo_id}", response_model=Todo)
+async def update_todo(todo_id: int, updated_todo: UpdateTodo):
+    for todo in TODOS:
+        if todo["id"] == todo_id:
+            updates = updated_todo.dict(exclude_unset=True)
+            todo.update(updates)
+            return todo
+    raise HTTPException(status_code=404, detail="Not found.")
+
+
+@app.delete("/todos/{todo_id}", status_code=204)
+async def delete_todo(todo_id: int):
+    for index, todo in enumerate(TODOS):
+        if todo["id"] == todo_id:
+            TODOS.pop(index)
+            return
+    raise HTTPException(status_code=404, detail="Not found")
