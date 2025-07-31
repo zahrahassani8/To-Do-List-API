db0eb3002f6738511b7f8101b9aa61182d767c63
zahrahassani8 <zahrahassanii08@gmail.com>
2025-07-31 17:29:47 +0330

    creating the base models

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional, List
 
 
 app = FastAPI()
+
+TODOS = [
+    {"id": 1, "title": "studying", "description": "for exams", "status": False},
+    {"id": 2, "title": "walking", "description": "on the street", "status": False},
+    {"id": 3, "title": "going to the gym",
+        "description": "for exercise", "status": False},
+    {"id": 4, "title": "reading", "description": "a book", "status": False},
+    {"id": 5, "title": "eating", "description": "healthy", "status": False},
+]
+
+
+class Todo(BaseModel):
+    id: int
+    title: str
+    description: str
+    status: bool
+
+
+class CreateTodo(BaseModel):
+    title: str
+    description: str
+    status: bool
+
+
+class UpdateTodo(BaseModel):
+    id: Optional[int] = None
+    title: Optional[str] = None
+    description: Optional[str] = None
+    status: Optional[bool] = None
 
 
 @app.get("/")
